7f8bdbc44c834b664b1b571e7ab51beb43427ccb
Yaakov <dbrionesmtz@yahoo.com>
2021-02-08 12:37:29 -0600

    Agregando el enlace para la seccion Todos los productos

--- views/categorias/index.php
+++ views/categorias/index.php
@@ -1,6 +1,7 @@
 <?php require 'views/header.php';?>
 <main class="contenedor pagina-categorias">
         <h2>Categorias</h2>
+        <a href="<?php echo URL.'todosProductos?pagina=1'; ?>">Ver todos los productos</a>
         <div class="grid-container categorias">
 
 <?php while($categorias=$this->categorias->fetch_assoc()){ ?>
